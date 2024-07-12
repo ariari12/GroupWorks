9682d7e60f8760e478c35a7fe458e925cab69b99
KAH <kahback99@gmail.com>
2024-07-12 16:10:57 +0900

    [feat] 엔티티 추가

---
+++ src/main/java/kr/co/groupworks/entity/kah/Calendar.java
@@ -0,0 +1,35 @@
+package kr.co.groupworks.entity.kah;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Calendar {
+    @Id
+    @GeneratedValue
+    private Long calendarId;
+    // 제목
+    private String title;
+    // 내용
+    private String contents;
+    // 일정 시작일
+    private String startDate;
+    // 일정 종료일
+    private String endDate;
+
+    // 사원 외래키
+
+    //휴가 외래키
+    @OneToMany(mappedBy = "calendar", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Vacation> vacations = new ArrayList<>();
+
+}
