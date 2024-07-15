a3441fbe621c8ab4f668968a7aaf4f0eec094327
KAH <kahback99@gmail.com>
2024-07-15 15:24:58 +0900

    [chore] querydsl 설정

--- src/main/java/kr/co/groupworks/control/kah/VacationController.java
+++ src/main/java/kr/co/groupworks/control/kah/VacationController.java
@@ -1,21 +1,32 @@
 package kr.co.groupworks.control.kah;
 
+import jakarta.servlet.http.HttpSession;
+import kr.co.groupworks.service.cis.EmployeeService;
+import kr.co.groupworks.service.kah.VacationService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
 @Controller
 @RequestMapping("/vacation")
+@RequiredArgsConstructor
 public class VacationController {
+    private final EmployeeService employeeService;
+    private final VacationService vacationService;
 
     @GetMapping("")
-    public String vacationMain(Model model) {
+    public String vacationMain(HttpSession session, Model model) {
         log.info("VacationController - vacationMain");
 
+        String employeeId = (String) session.getAttribute("employeeId");
+
         // 휴가 신청 내역 조회
+
         // 휴가 보유 사용현황 조회
 
 
