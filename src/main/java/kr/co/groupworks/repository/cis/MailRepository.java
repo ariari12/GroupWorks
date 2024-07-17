e6ff5f753ce7fa18e37daebe56c83fb1d2c59063
ILESONG <dlftjd9461@naver.com>
2024-07-18 01:10:45 +0900

    [chore] 메일 검색 후 폼 style 변경되는 것 올바르게 수정 및 검색할 때 검색 결과가 키워드를 완전 일치가 아니라 포함하는 것으로 변경

--- src/main/java/kr/co/groupworks/repository/cis/MailRepository.java
+++ src/main/java/kr/co/groupworks/repository/cis/MailRepository.java
@@ -9,18 +9,18 @@ import java.util.List;
 
 public interface MailRepository extends MongoRepository<Mail, String> {
 
-//    mail_status가 2면 휴지통에 있는 메일임
+//    mail_status = 0 : Default | 1 : 중요 | 2 : 휴지통
     
 //    받은 메일함에 사용하는 JPA
     @Query("{mail_receiver: ?0,'mail_status':{ $ne: 2 } }")
     List<Mail> findAllByMailReceiver(String receiverEmail);
 
 //    받은 메일함에서 제목으로 검색하는 JPA
-    @Query("{mail_receiver: ?0,'mail_title': ?1,'mail_status': {$ne: 2} }")
+    @Query("{mail_receiver: ?0,'mail_title': {$regex: ?1, $options: 'i'},'mail_status': {$ne: 2} }")
     List<Mail> findAllByMailReceiverAndMailTitle(String receiverEmail, String mailTitle);
 
 //    받은 메일함에서 보낸 사람으로 검색하는 JPA
-    @Query("{mail_receiver: ?0,'mail_sendername': ?1,'mail_status': {$ne: 2} }")
+    @Query("{mail_receiver: ?0,'mail_sendername': {$regex: ?1, $options: 'i'},'mail_status': {$ne: 2} }")
     List<Mail> findAllByMailReceiverAndMailSenderName(String receiverEmail, String mailSenderName);
 
 //  ==================================================================================================
@@ -30,17 +30,21 @@ public interface MailRepository extends MongoRepository<Mail, String> {
     List<Mail> findAllByMailSender(String senderEmail);
 
 //    보낸 메일함에서 제목으로 검색하는 JPA
-    @Query("{mail_sender: ?0,'mail_title': ?1,'mail_status': {$ne: 2}}")
+    @Query("{mail_sender: ?0,'mail_title': {$regex: ?1, $options: 'i'},'mail_status': {$ne: 2}}")
     List<Mail> findAllByMailSenderAndMailTitle(String senderEmail, String mailTitle);
 
 //    보낸 메일함에서 받은 사람으로 검색하는 JPA
-    @Query("{mail_sender: ?0,'mail_receivername': ?1,'mail_status': {$ne: 2}}")
+//    $regex는 2번째 파라미터를 정확하게 일치하는 것 뿐 아니라 포함하는지를 비교함, $options 'i'는 대소문자 구분 안하겠다는 것
+    @Query("{mail_sender: ?0,'mail_receivername': {$regex: ?1, $options: 'i'},'mail_status': {$ne: 2}}")
     List<Mail> findAllByMailSenderAndMailReceiverName(String senderEmail, String mailRecieverName);
+
+//  ==================================================================================================
 
 //    중요 메일함에 사용하는 JPA
     @Query("{ 'mail_receiver': ?0, 'mail_status': 1 }")
     List<Mail> findImportantByReceiver(String receiverEmail);
 
+//    휴지통 메일함에 사용하는 JPA
     @Query("{'mail_receiver' : ?0, 'mail_status' : 2}")
     List<Mail> findTrashByReceiver(String receiverEmail);
 
