ad123356210d8a75f8cb81945a9e136ad9a22e10
KAH <kahback99@gmail.com>
2024-07-22 17:21:29 +0900

    [feat] 기타 휴가 신청 기능 추가

--- src/main/java/kr/co/groupworks/dto/kah/OtherFormDTO.java
+++ src/main/java/kr/co/groupworks/dto/kah/OtherFormDTO.java
@@ -1,7 +1,13 @@
 package kr.co.groupworks.dto.kah;
 
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import kr.co.groupworks.entity.kah.VacationType;
 import lombok.*;
+
+import java.time.LocalDate;
 
 
 @Data
@@ -11,8 +17,14 @@ import lombok.*;
 public class OtherFormDTO {
     //남은 휴가, 휴일 일정, 상세 내용, 사원 번호,
     private Long employeeId;
-    private String otherStartDate;
-    private String otherEndDate;
+
+    @NotNull
+    @FutureOrPresent
+    private LocalDate otherStartDate;
+    @NotNull
+    @FutureOrPresent
+    private LocalDate otherEndDate;
+    @NotEmpty
     private String otherContents;
     private String otherFilePath;
     private String otherFileName;
