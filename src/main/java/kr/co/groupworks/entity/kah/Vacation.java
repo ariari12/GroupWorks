f0de3b0fab67593665a9392142a9f90ed995d062
KAH <kahback99@gmail.com>
2024-07-24 16:34:38 +0900

    [feat] 휴가 사용내역 조회 기능 추가

--- src/main/java/kr/co/groupworks/entity/kah/Vacation.java
+++ src/main/java/kr/co/groupworks/entity/kah/Vacation.java
@@ -5,6 +5,7 @@ import lombok.*;
 import lombok.experimental.SuperBuilder;
 
 @Entity
+@Table(name="vacation")
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
--- src/main/java/kr/co/groupworks/repository/cis/EmployeeRepository.java
+++ src/main/java/kr/co/groupworks/repository/cis/EmployeeRepository.java
@@ -1,12 +1,25 @@
 package kr.co.groupworks.repository.cis;
 
+import kr.co.groupworks.dto.kah.VacationMyHistoryDTO;
 import kr.co.groupworks.entity.cis.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
     public Employee findByEmployeeIdAndEmployeePWAndEmployeeName(Long emplyoeeId, String pw, String name);
 
     public Employee findByEmail(String email);
+
+    @Query("SELECT new kr.co.groupworks.dto.kah.VacationMyHistoryDTO" +
+            "(e.employeeId, e.employeeName, d.departmentName, e.annualDaysUsed, e.sickDaysUsed, e.otherDaysUsed)" +
+            " FROM Employee e" +
+            " JOIN e.department d"+
+            " WHERE e.employeeId = :employeeId AND e.employeeId = d.departmentId")
+    List<VacationMyHistoryDTO> findVacationMyHistoryDTO(@Param("employeeId") Long employeeId);
+
 
 }
