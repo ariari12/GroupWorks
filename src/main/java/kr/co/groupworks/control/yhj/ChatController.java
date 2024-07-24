06facbea5c753a41ad55e14e11d1531de1e1b8ce
HJ <hyun1996off@gmail.com>
2024-07-24 14:16:57 +0900

    [feat] 채팅 CSS, JS 추가.

---
+++ src/main/java/kr/co/groupworks/control/yhj/ChatController.java
@@ -0,0 +1,19 @@
+package kr.co.groupworks.control.yhj;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping("/chat")
+public class ChatController {
+
+    @GetMapping("/list")
+    public String chat(Model model) {
+        return "yhj/chat/list";
+    }
+}
