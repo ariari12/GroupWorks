6284a5332f1d11173fea03ae70e05304504b9923
KAH <kahback99@gmail.com>
2024-07-26 22:02:37 +0900

    [feat] 컬렉션 엔티티 -> dto 변환, 기타 휴가 신청 버그 수정

---
+++ src/main/java/kr/co/groupworks/dto/kah/VacationFileDTO.java
@@ -0,0 +1,16 @@
+package kr.co.groupworks.dto.kah;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class VacationFileDTO {
+    private String fileName;
+    // 실제 저장 파일 경로
+    private String filePath;
+}
