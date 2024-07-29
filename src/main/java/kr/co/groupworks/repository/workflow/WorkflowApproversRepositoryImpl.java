d11ecd3bedf25105a29c5526d18b0dd98386a356
sczzsccs <104063759+sczzsccs@users.noreply.github.com>
2024-07-29 17:27:17 +0900

    Merge branch 'JM-WorkFlow' of https://github.com/ariari12/GroupWorks into JM-WorkFlow
    # Conflicts:
    #	src/main/java/kr/co/groupworks/service/workflow/WorkFlowServiceImpl.java

--- src/main/java/kr/co/groupworks/repository/workflow/WorkflowApproversRepositoryImpl.java
+++ src/main/java/kr/co/groupworks/repository/workflow/WorkflowApproversRepositoryImpl.java
@@ -2,6 +2,8 @@ package kr.co.groupworks.repository.workflow;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
+import kr.co.groupworks.entity.workflow.QApproverEntity;
+import kr.co.groupworks.entity.workflow.QWorkFlowEntity;
 import kr.co.groupworks.entity.workflow.WorkFlowEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
