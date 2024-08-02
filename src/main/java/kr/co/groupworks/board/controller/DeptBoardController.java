b71260f474a570b147d641bea7df0583ee020d13
HJ <hyun1996off@gmail.com>
2024-08-02 17:50:28 +0900

    [feat] 부서게시판 리스트, 글쓰기, 디테일페이지 구현

--- src/main/java/kr/co/groupworks/board/controller/DeptBoardController.java
+++ src/main/java/kr/co/groupworks/board/controller/DeptBoardController.java
@@ -1,20 +1,29 @@
 package kr.co.groupworks.board.controller;
 
 
+import jakarta.servlet.http.HttpServletRequest;
+import kr.co.groupworks.board.dto.BoardDTO;
+import kr.co.groupworks.board.entity.Board;
+import kr.co.groupworks.board.entity.BoardStatus;
+import kr.co.groupworks.board.service.BoardService;
 import kr.co.groupworks.dto.cis.employee.SessionEmployeeDTO;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/board")
 public class DeptBoardController {
 
+    private final BoardService boardService;
+
+    // 게시판 리스트
     @GetMapping("{departmentId}/list")
     public String deptBoard(@PathVariable("departmentId") Long departmentId, Model model,
                         @SessionAttribute("employee") SessionEmployeeDTO sessionEmployeeDTO) {
@@ -22,6 +31,11 @@ public class DeptBoardController {
             return "redirect:/main?alert=invalid_department";
         }
         BoardUtils.setTitleAndType(model, BoardUtils.DEPARTMENT_BOARD, "");
+        List<BoardDTO> board = boardService.getAllBoards(departmentId);
+        for (BoardDTO b : board) {
+            b.setRelativeCreateDate(BoardUtils.formatRelativeTime(b.getCreateDate()));
+        }
+        model.addAttribute("board", board);
         return "board/list";
     }
 
@@ -31,19 +45,39 @@ public class DeptBoardController {
         if (!BoardUtils.hasAccessToDepartment(sessionEmployeeDTO, departmentId)) {
             return "redirect:/main?alert=invalid_department";
         }
-
         BoardUtils.setTitleAndType(model, BoardUtils.DEPARTMENT_BOARD, "글쓰기");
         return "board/write";
     }
 
+    @PostMapping("{departmentId}/write")
+    public String deptwrite(@PathVariable("departmentId") Long departmentId,
+                            @SessionAttribute("employee") SessionEmployeeDTO sessionEmployeeDTO,
+                            @ModelAttribute("boardDTO") BoardDTO dto,
+                            HttpServletRequest request) {
+
+        log.info("전달 받은 DTO : {}", dto);
+        dto.setIpAddress(request.getRemoteAddr());
+        dto.setDepartmentId(departmentId);
+        dto.setEmployeeId(sessionEmployeeDTO.getEmployeeId());
+
+        log.info("설정한 DTO : {}", dto);
+        boardService.register(dto);
+
+        return "redirect:/board/" + departmentId + "/list";
+    }
+
     @GetMapping("{departmentId}/detail/{no}")
-    public String deptDetail(@PathVariable("departmentId") Long departmentId, Model model,
+    public String deptDetail(@PathVariable("departmentId") Long departmentId,
+                             @PathVariable("no") Long no, Model model,
                          @SessionAttribute("employee") SessionEmployeeDTO sessionEmployeeDTO) {
         if (!BoardUtils.hasAccessToDepartment(sessionEmployeeDTO, departmentId)) {
             return "redirect:/main?alert=invalid_department";
         }
 
         BoardUtils.setTitleAndType(model, BoardUtils.DEPARTMENT_BOARD, "");
+        BoardDTO detail = boardService.getDetail(no, departmentId);
+
+        model.addAttribute("board", detail);
         return "board/detail";
     }
 
@@ -53,7 +87,6 @@ public class DeptBoardController {
         if (!BoardUtils.hasAccessToDepartment(sessionEmployeeDTO, departmentId)) {
             return "redirect:/main?alert=invalid_department";
         }
-
         BoardUtils.setTitleAndType(model, BoardUtils.DEPARTMENT_BOARD, "글수정");
         return "board/edit";
     }
