b559cc8a91718ccbc6bd9d4ac8d7aa009da05034
sczzsccs <104063759+sczzsccs@users.noreply.github.com>
2024-08-05 14:33:45 +0900

    [feat] materialflow newOrder.html 프론트 업데이트

--- src/main/java/kr/co/groupworks/materialflow/control/MaterialFlowManagerController.java
+++ src/main/java/kr/co/groupworks/materialflow/control/MaterialFlowManagerController.java
@@ -1,5 +1,7 @@
 package kr.co.groupworks.materialflow.control;
 
+import kr.co.groupworks.materialflow.entity.Business;
+import kr.co.groupworks.materialflow.service.MaterialOpenApiService;
 import kr.co.groupworks.materialflow.service.MaterialService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
--- src/main/java/kr/co/groupworks/materialflow/control/MaterialOpenAPI.java
+++ src/main/java/kr/co/groupworks/materialflow/control/MaterialOpenAPI.java
@@ -5,16 +5,16 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import kr.co.groupworks.materialflow.dto.BusinessDTO;
 import kr.co.groupworks.materialflow.entity.Business;
 import kr.co.groupworks.materialflow.service.MaterialOpenApiService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @RestController
