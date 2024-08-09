4103f4a254c2f314a6492dccee85dd582102c8fc
KAH <kahback99@gmail.com>
2024-08-10 03:51:48 +0900

    [refactor] 테스트 전체 파일 주석

--- src/test/java/kr/co/groupworks/security/SecurityConfigTest.java
+++ src/test/java/kr/co/groupworks/security/SecurityConfigTest.java
@@ -1,24 +1,24 @@
-package kr.co.groupworks.security;
-
-import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-@Slf4j
-@SpringBootTest
-class SecurityConfigTest {
-    @Autowired
-    private BCryptPasswordEncoder bCryptPasswordEncoder;
-
-    @Test @DisplayName("BCryptPasswordEncoder Test")
-    public void BCryptPasswordEncoderTest() {
-        final String password = "1111";
-        final String encode = bCryptPasswordEncoder.encode(password);
-
-        log.info("pw: {}, encode:{}", password, encode);
-    }
-
-}
+//package kr.co.groupworks.security;
+//
+//import lombok.extern.slf4j.Slf4j;
+//import org.junit.jupiter.api.DisplayName;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//
+//@Slf4j
+//@SpringBootTest
+//class SecurityConfigTest {
+//    @Autowired
+//    private BCryptPasswordEncoder bCryptPasswordEncoder;
+//
+//    @Test @DisplayName("BCryptPasswordEncoder Test")
+//    public void BCryptPasswordEncoderTest() {
+//        final String password = "1111";
+//        final String encode = bCryptPasswordEncoder.encode(password);
+//
+//        log.info("pw: {}, encode:{}", password, encode);
+//    }
+//
+//}
