89838d731094c48145817f6fe1e68bf088c2813f
KAH <kahback99@gmail.com>
2024-08-10 03:34:44 +0900

    [refactor] 테스트 실패 파일 주석처리

--- src/test/java/kr/co/groupworks/workflow/repository/EmployeeRepositoryTest.java
+++ src/test/java/kr/co/groupworks/workflow/repository/EmployeeRepositoryTest.java
@@ -1,21 +1,21 @@
-//package kr.co.groupworks.workflow.repository;
-//
-//import kr.co.groupworks.department.repository.DepartmentRepository;
-//import lombok.extern.slf4j.Slf4j;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.transaction.annotation.Transactional;
-//
-//@Slf4j
-//@SpringBootTest
-//@Transactional
-//class EmployeeRepositoryTest {
-//    @Autowired
-//    DepartmentRepository departmentRepository;
-//
-//    @Test
-//    void getDepartments() {
-//        departmentRepository.getDepartments().forEach(o -> log.info("{}", o));
-//    }
-//}
+package kr.co.groupworks.workflow.repository;
+
+import kr.co.groupworks.department.repository.DepartmentRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@SpringBootTest
+@Transactional
+class EmployeeRepositoryTest {
+    @Autowired
+    DepartmentRepository departmentRepository;
+
+    @Test
+    void getDepartments() {
+        departmentRepository.getDepartments().forEach(o -> log.info("{}", o));
+    }
+}
