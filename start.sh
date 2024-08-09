138c6164dcc932d1e947ad54e9505e6de1230a70
KAH <kahback99@gmail.com>
2024-08-09 16:17:16 +0900

    [save] aws 배포중 5

---
+++ start.sh
@@ -0,0 +1,18 @@
+#start.sh
+  docker_username=""
+  image_name="app"
+  container_name="web"
+  port=80
+
+  #remove container
+  echo "==> Remove previous container ..."
+  docker rm -f ${container_name}
+
+  echo "==> Remove previous image .."
+  docker rmi -f ${image_name}
+
+  echo "==> create new Image "
+  docker build -t ${image_name} -f Dockerfile .
+
+  echo "==> Run container"
+  docker run -d -p ${port}:${port} --name ${container_name} ${image_name}
