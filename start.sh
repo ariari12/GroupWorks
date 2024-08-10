a5fdacc10c1577cf3f58e45350e254520c76379d
KAH <kahback99@gmail.com>
2024-08-11 04:41:18 +0900

    [chore] 도커 컴포즈로 배포5

--- start.sh
+++ start.sh
@@ -7,9 +7,9 @@ docker-compose down
 # 강제로 redis 및 web 컨테이너 제거
 docker rm -f redis web || true
 
-# Build and start the new containers
+# Build and start the new containers with init to handle zombie processes
 echo "==> Building and starting new containers with Docker Compose..."
-docker-compose up --build -d
+docker-compose up --build -d --force-recreate --remove-orphans --init
 
 
 
