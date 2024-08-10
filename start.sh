ca4a61362ea181c115edd5e753d16b5abde21ca2
KAH <kahback99@gmail.com>
2024-08-11 04:34:07 +0900

    [chore] 도커 컴포즈로 배포4

--- start.sh
+++ start.sh
@@ -4,6 +4,13 @@
 echo "==> Removing previous containers..."
 docker-compose down
 
+# 강제로 redis 및 web 컨테이너 제거
+docker rm -f redis web || true
+
 # Build and start the new containers
 echo "==> Building and starting new containers with Docker Compose..."
 docker-compose up --build -d
+
+
+
+
