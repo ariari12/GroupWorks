a25dce02a9c4d3de77ab7f83b4b7ee081d6a98c3
KAH <kahback99@gmail.com>
2024-08-11 23:24:30 +0900

    [feat] 알림 전체조회 및 캐싱 설정

--- src/main/java/kr/co/groupworks/notification/service/NotificationServiceImpl.java
+++ src/main/java/kr/co/groupworks/notification/service/NotificationServiceImpl.java
@@ -9,10 +9,13 @@ import kr.co.groupworks.notification.repository.NotificationRepository;
 import kr.co.groupworks.notification.sse.NotificationSseEmitter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 @Slf4j
@@ -27,6 +30,7 @@ public class NotificationServiceImpl implements NotificationService {
         return notificationRepository.saveWithTTL(notification, timeout, timeUnit);
     }
 
+    @CacheEvict(value = "notificationCache", key = "#vacation.employee.employeeId")
     public void sendVacationApproval(Vacation vacation, Employee sender) {
         Notification notification = Notification.builder()
                 .title("휴가 신청 알림")
@@ -44,5 +48,12 @@ public class NotificationServiceImpl implements NotificationService {
         );
         log.info(dto.toString());
         notificationSseEmitter.sendNotification(vacation.getEmployee().getEmployeeId(), dto);
+    }
+
+    @Cacheable(value = "notificationCache", key = "#receiverId")
+    public List<NotificationDTO> getAllNotificationsByReceiverId(Long receiverId) {
+        return notificationRepository.findAllByReceiverId(receiverId)
+                .stream().map(notificationMapper::toDto)
+                .toList();
     }
 }
--- src/main/java/kr/co/groupworks/notification/sse/NotificationSseEmitter.java
+++ src/main/java/kr/co/groupworks/notification/sse/NotificationSseEmitter.java
@@ -2,6 +2,8 @@ package kr.co.groupworks.notification.sse;
 
 import kr.co.groupworks.notification.dto.NotificationDTO;
 import kr.co.groupworks.notification.model.Notification;
+import kr.co.groupworks.notification.service.NotificationService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
@@ -12,20 +14,30 @@ import java.util.concurrent.ConcurrentHashMap;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class NotificationSseEmitter {
     private final Map<Long, SseEmitter> sseEmitters = new ConcurrentHashMap<>();
 
+    // 사용자 연결 처리
     public SseEmitter connect(Long employeeId) {
         SseEmitter sseEmitter = new SseEmitter(Long.MAX_VALUE);
         sseEmitters.put(employeeId, sseEmitter);
+
+        // 연결 완료 시 사용자 제거
         sseEmitter.onCompletion(() -> sseEmitters.remove(employeeId));
+
+        // 연결 시간 초과 시 사용자 제거
         sseEmitter.onTimeout(() -> sseEmitters.remove(employeeId));
+
         log.info("NotificationSseEmitter employee {}", employeeId);
         return sseEmitter;
     }
 
+    // 알림 전송
     public void sendNotification(Long receiverId, NotificationDTO notificationDTO) {
         SseEmitter sseEmitter = sseEmitters.get(receiverId);
+
+        // SSE 연결이 있는 경우에만 알림 전송
         if (sseEmitter != null) {
             try {
                 sseEmitter.send(notificationDTO);
