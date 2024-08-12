3882bb67aa23ccf546bb9986a8290ac5e0def89d
KAH <kahback99@gmail.com>
2024-08-12 17:45:03 +0900

    [chore] test 주석 해제

--- src/test/java/kr/co/groupworks/GroupWorksApplicationTests.java
+++ src/test/java/kr/co/groupworks/GroupWorksApplicationTests.java
@@ -1,13 +1,13 @@
-package kr.co.groupworks;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class GroupWorksApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package kr.co.groupworks;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class GroupWorksApplicationTests {
+//
+////    @Test
+//    void contextLoads() {
+//    }
+//
+//}
