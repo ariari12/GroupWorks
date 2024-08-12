953c2d8df9dea1391f947853e3cc33198504653f
KAH <kahback99@gmail.com>
2024-08-12 15:32:44 +0900

    [chore] coolsms 설정 변경 6

--- src/main/java/kr/co/groupworks/config/CoolSmsConfig.java
+++ src/main/java/kr/co/groupworks/config/CoolSmsConfig.java
@@ -8,8 +8,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.io.IOException;
-
 @Slf4j
 @Configuration
 public class CoolSmsConfig {
