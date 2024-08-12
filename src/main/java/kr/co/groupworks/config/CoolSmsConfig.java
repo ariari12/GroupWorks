9e412e5179b66b253dd71b0c3bc56664b1c39272
KAH <kahback99@gmail.com>
2024-08-12 17:24:09 +0900

    [chore] coolsms설정 주석 해제

--- src/main/java/kr/co/groupworks/config/CoolSmsConfig.java
+++ src/main/java/kr/co/groupworks/config/CoolSmsConfig.java
@@ -1,42 +1,39 @@
-//package kr.co.groupworks.config;
-//
-//import lombok.extern.slf4j.Slf4j;
-//import net.nurigo.sdk.NurigoApp;
-//import net.nurigo.sdk.message.service.DefaultMessageService;
-//import org.jasypt.util.text.AES256TextEncryptor;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//@Slf4j
-//@Configuration
-//public class CoolSmsConfig {
-//    @Value("${coolsms.api.dmain.url}")
-//    private String smsApiDomain;
-//    @Value("${coolsms.encryptor.key.property}")
-//    private String keyProperty;
-//    @Value("${coolsms.api-key}")
-//    private String apiKey;
-//    @Value("${coolsms.secret-key}")
-//    private String apiSecretKey;
-//
-////    private AES256TextEncryptor encryptor;
-//
-//    @Bean
-//    public DefaultMessageService encodeApiKeyCreateMessageService() {
-////        String key = encryptor.decrypt(apiKey);
-////        String secret = encryptor.decrypt(apiSecretKey);
-//
-////        log.info("secret : " + secret);
-////        log.info("key" + key);
-//
-//        return NurigoApp.INSTANCE.initialize(apiKey, apiSecretKey, smsApiDomain);
-//    }
-//
-////    @Bean
-////    public AES256TextEncryptor setEncryptor(){
-////        encryptor = new AES256TextEncryptor();
-////        encryptor.setPassword(keyProperty);
-////        return encryptor;
-////    }
-//}
+package kr.co.groupworks.config;
+
+import lombok.extern.slf4j.Slf4j;
+import net.nurigo.sdk.NurigoApp;
+import net.nurigo.sdk.message.service.DefaultMessageService;
+import org.jasypt.util.text.AES256TextEncryptor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
+public class CoolSmsConfig {
+    @Value("${coolsms.api.dmain.url}")
+    private String smsApiDomain;
+    @Value("${coolsms.encryptor.key.property}")
+    private String keyProperty;
+    @Value("${coolsms.api-key}")
+    private String apiKey;
+    @Value("${coolsms.secret-key}")
+    private String apiSecretKey;
+
+    private AES256TextEncryptor encryptor;
+
+    @Bean
+    public DefaultMessageService encodeApiKeyCreateMessageService() {
+        String key = encryptor.decrypt(apiKey);
+        String secret = encryptor.decrypt(apiSecretKey);
+
+        return NurigoApp.INSTANCE.initialize(key, secret, smsApiDomain);
+    }
+
+    @Bean
+    public AES256TextEncryptor setEncryptor(){
+        encryptor = new AES256TextEncryptor();
+        encryptor.setPassword(keyProperty);
+        return encryptor;
+    }
+}
