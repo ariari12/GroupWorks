70a58ae504ecddad200d2adc77002a894c47cfba
KAH <kahback99@gmail.com>
2024-08-13 10:05:02 +0900

    [chore] 테스트 주석, 레디스 비번 추가

--- src/test/java/kr/co/groupworks/EmployeeTest.java
+++ src/test/java/kr/co/groupworks/EmployeeTest.java
@@ -86,4 +86,3 @@ public class EmployeeTest {
         vacationHistoryRepository.saveAll(vacationHistories);
     }
 }
-
--- src/test/java/kr/co/groupworks/GroupWorksApplicationTests.java
+++ src/test/java/kr/co/groupworks/GroupWorksApplicationTests.java
@@ -1,13 +1,13 @@
-package kr.co.groupworks;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class GroupWorksApplicationTests {
-
+//package kr.co.groupworks;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class GroupWorksApplicationTests {
+//
 //    @Test
-    void contextLoads() {
-    }
-
-}
+//    void contextLoads() {
+//    }
+//
+//}
