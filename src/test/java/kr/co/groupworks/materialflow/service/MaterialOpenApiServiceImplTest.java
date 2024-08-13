a1381ba7016deb3eda7838b117eb29f90c03a036
KAH <kahback99@gmail.com>
2024-08-13 09:49:36 +0900

    [chore] 테스트 주석해제2

--- src/test/java/kr/co/groupworks/materialflow/service/MaterialOpenApiServiceImplTest.java
+++ src/test/java/kr/co/groupworks/materialflow/service/MaterialOpenApiServiceImplTest.java
@@ -1,30 +1,30 @@
-//package kr.co.groupworks.materialflow.service;
-//
-//import lombok.extern.slf4j.Slf4j;
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.transaction.annotation.Transactional;
-//import org.springframework.util.StopWatch;
-//
-//@Slf4j
-//@SpringBootTest
-//@Transactional
-//class MaterialOpenApiServiceImplTest {
-//    @Autowired
-//    private MaterialService materialService;
-//
-//    @Test @DisplayName("Bom Order List FindAll QueryDsl Test")
-//    void getBomList() {
-//        StopWatch st = new StopWatch();
-//        st.start();
-//        materialService.getBomList().forEach(o -> {
-//            log.info("Order Id: {}", o.getId());
-//            log.info("Order Bom List =======");
-//            o.getBomList().forEach(b -> log.info("  Bom : {}", b));
-//        });
-//        st.stop();
-//        log.info(st.prettyPrint());
-//    }
-//}
+package kr.co.groupworks.materialflow.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StopWatch;
+
+@Slf4j
+@SpringBootTest
+@Transactional
+class MaterialOpenApiServiceImplTest {
+    @Autowired
+    private MaterialService materialService;
+
+    @Test @DisplayName("Bom Order List FindAll QueryDsl Test")
+    void getBomList() {
+        StopWatch st = new StopWatch();
+        st.start();
+        materialService.getBomList().forEach(o -> {
+            log.info("Order Id: {}", o.getId());
+            log.info("Order Bom List =======");
+            o.getBomList().forEach(b -> log.info("  Bom : {}", b));
+        });
+        st.stop();
+        log.info(st.prettyPrint());
+    }
+}
