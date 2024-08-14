2a3d78e91590a03dbf62066361b62a72ad66e341
KAH <kahback99@gmail.com>
2024-08-14 16:20:23 +0900

    [chore] 휴가 전용 Advice 컨트롤러 추가

--- src/main/java/kr/co/groupworks/common/exception/VacationControllerAdvice.java
+++ src/main/java/kr/co/groupworks/common/exception/VacationControllerAdvice.java
@@ -5,23 +5,29 @@ import kr.co.groupworks.common.exception.exhandler.ErrorResult;
 import kr.co.groupworks.common.exception.exhandler.NotEnoughLeaveDaysException;
 import kr.co.groupworks.common.exception.exhandler.VacationNotPendingException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @Slf4j
-@RestControllerAdvice
+@ControllerAdvice(basePackages = "kr.co.groupworks.calendar") // 특정 패키지에만 적용
+@Order(1)
 public class VacationControllerAdvice {
+
     @ExceptionHandler(VacationNotPendingException.class)
     public ModelAndView handleVacationNotPendingException(VacationNotPendingException ex) {
         ModelAndView modelAndView = new ModelAndView("error/401");
         modelAndView.setStatus(HttpStatus.UNAUTHORIZED);
         modelAndView.addObject("message", ex.getMessage());
+        log.info(modelAndView.toString());
         return modelAndView;
     }
 
+    @ResponseBody
     @ExceptionHandler(NotEnoughLeaveDaysException.class)
     public ResponseEntity<ErrorResult> handleNotEnoughLeaveDaysException(NotEnoughLeaveDaysException ex) {
         log.error("[handleNotEnoughLeaveDaysException] ex", ex);
