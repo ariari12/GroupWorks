ac3cd2d303fd6bec2c7fe09235fbbf7f7d148ff3
KAH <kahback99@gmail.com>
2024-08-19 09:32:26 +0900

    [chore] 테스트 주석 해제

--- src/main/java/kr/co/groupworks/common/aop/PerformanceAspect.java
+++ src/main/java/kr/co/groupworks/common/aop/PerformanceAspect.java
@@ -14,36 +14,17 @@ public class PerformanceAspect {
 
     @Around("execution(* kr.co.groupworks..*(..))")
     public Object logExecutionTimeByDomainAndLayer(ProceedingJoinPoint joinPoint) throws Throwable {
-        String packageName = joinPoint.getSignature().getDeclaringTypeName();
-
-        String[] packageParts = packageName.split("\\.");
-
-        String domain = "unknownDomain";
-        String layer = "unknownLayer";
-
-        if (packageParts.length >= 4) {
-            domain = packageParts[3];  // 도메인 추출 (예: calendar, notification 등)
-        } else {
-            log.warn("패키지 경로가 예상보다 짧습니다: " + packageName);
-        }
-
-        if (packageParts.length >= 5) {
-            layer = packageParts[4];   // 계층 추출 (예: controller, service, repository)
-        } else {
-            log.warn("패키지 경로가 예상보다 짧습니다: " + packageName);
-        }
-
-        return logExecutionTime(joinPoint, domain, layer);
+        return logExecutionTime(joinPoint);
     }
 
     // 공통 성능 측정 로직
-    private Object logExecutionTime(ProceedingJoinPoint joinPoint, String domain, String layer) throws Throwable {
+    private Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
         long start = System.currentTimeMillis();
 
         Object proceed = joinPoint.proceed();
 
         long executionTime = System.currentTimeMillis() - start;
-        log.info("[{} Domain - {} Layer] {} executed in {} ms", domain, layer, joinPoint.getSignature(), executionTime);
+        log.info("{} executed in {} ms", joinPoint.getSignature(), executionTime);
 
         return proceed;
     }
