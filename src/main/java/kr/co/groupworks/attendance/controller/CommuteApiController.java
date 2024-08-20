4fab19e92f086ca9a80b66a30ad98a049383711f
HJ <hyun1996off@gmail.com>
2024-08-20 15:23:55 +0900

    [chore] 근태관리 구현

--- src/main/java/kr/co/groupworks/attendance/controller/CommuteApiController.java
+++ src/main/java/kr/co/groupworks/attendance/controller/CommuteApiController.java
@@ -1,9 +1,12 @@
 package kr.co.groupworks.attendance.controller;
 
 import kr.co.groupworks.attendance.dto.AttendanceDTO;
+import kr.co.groupworks.attendance.dto.AttendanceModificationDTO;
+import kr.co.groupworks.attendance.service.AttendanceModificationService;
 import kr.co.groupworks.attendance.service.AttendanceService;
 import kr.co.groupworks.employee.dto.SessionEmployeeDTO;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,12 +16,14 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/commute")
 public class CommuteApiController {
 
     private final AttendanceService attendanceService;
+    private final AttendanceModificationService attendanceModificationService;
 
     // 리스트
     @GetMapping("/list")
@@ -88,23 +93,47 @@ public class CommuteApiController {
 
     // 근태 추가
     @PostMapping("/add-requests")
-    public ResponseEntity<?> addRequests(@SessionAttribute("employee") SessionEmployeeDTO sessionEmployeeDTO) {
+    public ResponseEntity<?> addRequests(@SessionAttribute("employee") SessionEmployeeDTO sessionEmployeeDTO,
+                                         @RequestBody AttendanceDTO dto) {
 
-        return ResponseEntity.badRequest().body("null");
+        try {
+            Long employeeId = sessionEmployeeDTO.getEmployeeId();
+            dto.setEmployeeId(employeeId);
+            attendanceService.addAttendance(dto);
+            return ResponseEntity.ok("근태추가 요청이 성공적으로 신청되었습니다.");
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body("이미 근태 기록이 존재합니다.");
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("신청도중 오류가 발생하였습니다.");
+        }
     }
 
     // 근태 수정
     @PutMapping("/edit-requests")
-    public ResponseEntity<?> editRequests(@SessionAttribute("employee") SessionEmployeeDTO sessionEmployeeDTO) {
+    public ResponseEntity<?> editRequests(@SessionAttribute("employee") SessionEmployeeDTO sessionEmployeeDTO,
+                                          @RequestBody AttendanceModificationDTO dto) {
 
-        return ResponseEntity.badRequest().body("null");
+        Long employeeId = sessionEmployeeDTO.getEmployeeId();
+        dto.setEmployeeId(employeeId);
+
+        attendanceModificationService.modifyAttendance(dto);
+        return ResponseEntity.ok("근태 수정에 성공하였습니다.");
     }
 
     // 근태 요청 리스트
     @GetMapping("/view-requests")
     public ResponseEntity<?> viewCommuteRequests(@SessionAttribute("employee") SessionEmployeeDTO sessionEmployeeDTO) {
 
-        return ResponseEntity.badRequest().body("null");
+        try {
+            Long departmentId = sessionEmployeeDTO.getDepartment().getDepartmentId();
+            List<AttendanceModificationDTO> requests = attendanceModificationService.
+                    getModificationsByDepartmentId(departmentId);
+
+
+            return ResponseEntity.ok(requests);
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 
     // 근태 요청 처리
--- src/main/java/kr/co/groupworks/attendance/repository/AttendanceRepository.java
+++ src/main/java/kr/co/groupworks/attendance/repository/AttendanceRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
@@ -14,6 +13,13 @@ import java.util.Optional;
 public interface AttendanceRepository extends JpaRepository<Attendance, Long> {
     List<Attendance> findAllByEmployee_EmployeeIdAndDateBetween(Long employeeId, LocalDateTime startDate, LocalDateTime endDate);
 
+
+    @Query("SELECT a FROM Attendance a WHERE a.employee.id = :employeeId AND a.date BETWEEN :startDate AND :endDate")
+    List<Attendance> findAttendancesByEmployeeIdAndDateRange(
+            @Param("employeeId") Long employeeId,
+            @Param("startDate") LocalDateTime startDate,
+            @Param("endDate") LocalDateTime endDate
+    );
 
     @Query("SELECT a FROM Attendance a WHERE a.employee.id = :employeeId AND a.clockInTime BETWEEN :startOfDay AND :endOfDay")
     Optional<Attendance> findClockInRecordForToday(
