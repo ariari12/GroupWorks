70362c2b1528a882a6848588111e78ebf931c309
HJ <hyun1996off@gmail.com>
2024-08-21 16:45:42 +0900

    [fix] 메인공지사항 부서이름이 조회가 안되된점 수정.

--- src/main/java/kr/co/groupworks/board/entity/Board.java
+++ src/main/java/kr/co/groupworks/board/entity/Board.java
@@ -14,6 +14,7 @@ import java.time.LocalDateTime;
 @Entity
 @Getter
 @Setter
+@ToString
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
@@ -65,8 +66,7 @@ public class Board{
     @JoinColumn(name = "employee_id", nullable = false)
     private Employee employee;
 
-    // @ManyToOne(fetch = FetchType.LAZY)
-    @ManyToOne()
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "department_id")
     private Department department;
 
